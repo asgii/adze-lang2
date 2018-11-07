115ccf04ef413bbcd9163790268ba03deaedf128
asgii <32249254+asgii@users.noreply.github.com>
2018-11-07 18:33:45 +0000

    Discard commented out tokens

--- src/lex.rs
+++ src/lex.rs
@@ -25,6 +25,12 @@ pub enum TokenKind {
     OthInvalid,
 }
 
+#[derive (Debug, Copy, Clone)]
+enum LexerState {
+    Lexing,
+    MidComment,
+}
+
 // @TODO better way of doing this
 //
 fn parse_map() -> HashMap<&'static str, TokenKind> {
@@ -48,6 +54,7 @@ fn parse_map() -> HashMap<&'static str, TokenKind> {
     parse_map
 }
 
+#[derive (Debug)]
 pub struct Token<'a> {
     kind: TokenKind,
     source: &'a str,
@@ -91,10 +98,14 @@ impl Lexer {
         //
         let lines = source.lines();
         for line in lines {
-            let words = line.split_whitespace();
+            let mut state = LexerState::Lexing;
 
+            let words = line.split_whitespace();
             for word in words {
-                tokens.push(self.lex_word(word));
+
+                if let Some(token) = self.lex_word(word, &mut state) {
+                    tokens.push(token);
+                }
             }
         }
 
@@ -103,13 +114,51 @@ impl Lexer {
 
     fn lex_word<'a>(
         &self,
-        word: &'a str
-    ) -> Token<'a> {
-        match self.parse_map.get(word) {
+        word: &'a str,
+        state: &mut LexerState,
+    ) -> Option<Token<'a>> {
+        let token = match self.parse_map.get(word) {
             Some(kind) => Token::new(*kind, word),
             None => {
                 // @TODO check name, otherwise invalid
                 Token::new(TokenKind::OthInvalid, word)
+            },
+        };
+
+        let (token, next_state) = Self::handle_comments(token, *state);
+
+        *state = next_state;
+
+        token
+    }
+
+    /// Decide whether or not to keep `token`, given `state`, the state of
+    /// comment lexing.
+    ///
+    /// Returns the new state along with the presence or absence of `token`.
+    ///
+    fn handle_comments<'a>(
+        token: Token<'a>,
+        state: LexerState,
+    ) -> (Option<Token<'a>>, LexerState) {
+        match (&state, &token.kind) {
+            // Start ignoring on comment
+            (&LexerState::Lexing, &TokenKind::GramComment) => {
+                (None, LexerState::MidComment)
+            },
+
+            // Stop ignoring on end of comment
+            (&LexerState::MidComment, &TokenKind::GramComment) => {
+                (None, LexerState::Lexing)
+            },
+
+            // Ignore when mid-comment
+            (&LexerState::MidComment, _) => {
+                (None, LexerState::MidComment)
+            },
+
+            (&LexerState::Lexing, _) => {
+                (Some(token), LexerState::Lexing)
             },
         }
     }

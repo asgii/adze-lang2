82d975e112e649295326a43ee1d33411e8d5f90b
asgii <32249254+asgii@users.noreply.github.com>
2018-11-10 10:30:25 +0000

    Eat whitespace. Use that as a criterion for validity of symbol placement.
    The TokenIter has to iterate over a line, not a word, for this to work. This is inevitable if whitespace is to be meaningful (currently, I am imagining enforcing the whitespace between 'a + b').

--- src/lex.rs
+++ src/lex.rs
@@ -30,28 +30,73 @@ pub enum TokenKind {
     OthInvalid,
 }
 
-#[derive (Debug, Copy, Clone)]
-enum LexerState {
-    Lexing,
-    MidComment,
+/// Whether a token can validly follow immediately after the last one without
+/// a line-ending or whitespace.
+///
+/// e.g., `return;` is two valid tokens, because `;` can follow immediately.
+///
+enum TokenCanFollowImmediately {
+    Can,
+    Cannot,
 }
 
-static SYMBOLS: phf::Map<&'static str, TokenKind> = phf_map! {
-    "," => TokenKind::GramComma,
-    ";" => TokenKind::GramSemicolon,
-    ":" => TokenKind::GramColon,
-    "#" => TokenKind::GramComment,
+struct TokenProperties {
+    pub kind: TokenKind,
+    pub can_follow: TokenCanFollowImmediately,
+}
 
-    "=" => TokenKind::OpAssign,
-    "+" => TokenKind::OpAdd,
-    "-" => TokenKind::OpSub,
+static SYMBOLS: phf::Map<&'static str, TokenProperties> = phf_map! {
+    "," => TokenProperties {
+        kind: TokenKind::GramComma,
+        can_follow: TokenCanFollowImmediately::Can
+    },
+    ";" => TokenProperties {
+        kind: TokenKind::GramSemicolon,
+        can_follow: TokenCanFollowImmediately::Can,
+    },
+    ":" => TokenProperties {
+        kind: TokenKind::GramColon,
+        can_follow: TokenCanFollowImmediately::Can,
+    },
+    "#" => TokenProperties {
+        kind: TokenKind::GramComment,
+        can_follow: TokenCanFollowImmediately::Can,
+    },
 
-    "{" => TokenKind::BraceOpen,
-    "}" => TokenKind::BraceClose,
-    "(" => TokenKind::ParenOpen,
-    ")" => TokenKind::ParenClose,
+    "=" => TokenProperties {
+        kind: TokenKind::OpAssign,
+        can_follow: TokenCanFollowImmediately::Cannot,
+    },
+    "+" => TokenProperties {
+        kind: TokenKind::OpAdd,
+        can_follow: TokenCanFollowImmediately::Cannot,
+    },
+    "-" => TokenProperties {
+        kind: TokenKind::OpSub,
+        can_follow: TokenCanFollowImmediately::Cannot,
+    },
+
+    "{" => TokenProperties {
+        kind: TokenKind::BraceOpen,
+        can_follow: TokenCanFollowImmediately::Cannot,
+    },
+    "}" => TokenProperties {
+        kind: TokenKind::BraceClose,
+        can_follow: TokenCanFollowImmediately::Cannot,
+    },
+    "(" => TokenProperties {
+        kind: TokenKind::ParenOpen,
+        can_follow: TokenCanFollowImmediately::Can,
+    },
+    ")" => TokenProperties {
+        kind: TokenKind::ParenClose,
+        can_follow: TokenCanFollowImmediately::Can,
+    },
 };
 
+/// Key-words are kept separately from symbols because the former must be
+/// delimited from other tokens, where the latter have exceptions.
+///
 static KEYWORDS: phf::Map<&'static str, TokenKind> = phf_map! {
     "return" => TokenKind::KeyReturn,
     "if"     => TokenKind::KeyIf,
@@ -77,14 +122,65 @@ impl<'a> Token<'a> {
     }
 }
 
+enum WhitespaceState {
+    StartOfLine,
+    Whitespace,
+    NoWhitespace,
+    EndOfLine,
+}
+
 struct TokenIter<'a> {
     word: &'a str,
+
+    /// Whether there was whitespace (or the start or end to a line) immediately
+    /// before the start of `word`.
+    whitespace: WhitespaceState,
 }
 
 impl <'a> TokenIter<'a> {
     pub fn new(word: &'a str) -> TokenIter<'a> {
         TokenIter {
             word,
+            whitespace: WhitespaceState::StartOfLine,
+        }
+    }
+
+    /// Advance past any whitespace; return whether there was any whitespace.
+    ///
+    fn eat_whitespace(&mut self) {
+        let was_whitespace = match self.word.bytes().next() {
+            None => WhitespaceState::EndOfLine,
+            Some(first_char) => match first_char.is_ascii_whitespace() {
+                true  => WhitespaceState::Whitespace,
+                false => WhitespaceState::NoWhitespace,
+            },
+        };
+
+        let mut len_whitespace = 0;
+        for this_char in self.word.bytes() {
+            if this_char.is_ascii_whitespace() {
+                len_whitespace += 1;
+            }
+            else {
+                break;
+            }
+        }
+        self.word = &self.word[len_whitespace..];
+
+        // To have this function encapsulate whitespace-eating completely, it
+        // must handle the start of the line. We want to eat whitespace at the
+        // start of the line, but unlike other cases, we don't want to return
+        // whether there was whitespace, since, for the purposes of symbol
+        // validity, the start of the line should count as whitespace!
+        //
+        // So we treat the start specially, ignoring any lack of whitespace when
+        // setting `whitespace`.
+        //
+        if let WhitespaceState::StartOfLine = self.whitespace {
+            self.whitespace = WhitespaceState::Whitespace;
+        }
+        else {
+            self.whitespace = was_whitespace;
         }
     }
 }
@@ -93,24 +189,44 @@ impl <'a> Iterator for TokenIter<'a> {
     type Item = Token<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        // Previous iterations will have advanced us just past the end of the
+        // last word (or line).
+        //
+        self.eat_whitespace();
+
         // The only case where we stop iterating is when we run out of word.
         //
         if self.word.len() == 0 {
             return None;
         }
 
-        // @TODO make generic fn of this
-        //
-        for (symbol, token) in SYMBOLS.entries() {
+        // Try to match a symbol or key-word.
+
+        for (symbol, TokenProperties {
+            kind, can_follow,
+        }) in SYMBOLS.entries() {
             if symbol.len() > self.word.len() {
                 continue;
             }
+            // Note Range for str is in bytes, not chars (as is len()).
             let trunc = &self.word[..symbol.len()];
 
             if *symbol == trunc {
                 self.word = &self.word[symbol.len()..];
 
-                return Some(Token::new(*token, trunc));
+                return match self.whitespace {
+                    WhitespaceState::NoWhitespace => {
+                        // Some symbols are allowed without whitespace
+                        // immediately before them; we have to do this
+                        // additional check.
+                        //
+                        match can_follow {
+                            TokenCanFollowImmediately::Can => Some(Token::new(*kind, trunc)),
+                            TokenCanFollowImmediately::Cannot => Some(Token::new(TokenKind::OthInvalid, trunc)),
+                        }
+                    },
+                    _ => Some(Token::new(*kind, trunc)),
+                };
             }
         }
 
@@ -121,9 +237,38 @@ impl <'a> Iterator for TokenIter<'a> {
             let trunc = &self.word[..keyword.len()];
 
             if *keyword == trunc {
+                // This could still be a name, e.g. main_a.
+                // We must check what comes afterward.
+                //
+                let after_keyword = &self.word[keyword.len()..];
+                if let Some(next_char) = after_keyword.bytes().next() {
+                    // We have to check more than just whitespace, because of
+                    // cases like main().
+                    // Effectively we're checking for in principle validity of a
+                    // character within a name - not actual validity, which will
+                    // be checked by eat_name().
+                    //
+                    if next_char == '_' as u8 ||
+                       next_char.is_ascii_alphanumeric() {
+                        // continue, not break, in case some symbols contain
+                        // others, as e.g. += contains +.
+                        continue;
+                    }
+                }
+
                 self.word = &self.word[keyword.len()..];
 
-                return Some(Token::new(*token, trunc));
+                // Key-words must be preceded by whitespace; this determines
+                // validity as a key-word.
+                //
+                // @OPTION technically this is backwards; we could have checked
+                // higher up whether there was whitespace, and if not, only
+                // matched against symbols.
+                //
+                return match self.whitespace {
+                    WhitespaceState::NoWhitespace => Some(Token::new(TokenKind::OthInvalid, trunc)),
+                    _ => Some(Token::new(*token, trunc)),
+                };
             }
         }
 
@@ -138,6 +283,14 @@ impl <'a> Iterator for TokenIter<'a> {
         self.word = "";
         Some(Token::new(TokenKind::OthInvalid, remaining))
     }
+}
+
+/// The state of lexing with regard to comments.
+///
+#[derive (Debug, Copy, Clone)]
+enum LexerState {
+    Lexing,
+    MidComment,
 }
 
 pub struct Lexer {
@@ -165,21 +318,15 @@ impl Lexer {
         for line in lines {
             let mut state = LexerState::Lexing;
 
-            let words = line.split_whitespace();
-            for word in words {
-                let mut iter = TokenIter::new(word);
+            let mut iter = TokenIter::new(line);
 
-                // @TODO this is not exactly right.
+            while let Some(token) = iter.next() {
+                let (new_token, new_state) = Self::handle_comments(token, state);
 
-                while let Some(token) = iter.next() {
-                    let (new_token, new_state) =
-                        Self::handle_comments(token, state);
+                state = new_state;
 
-                    state = new_state;
-
-                    if let Some(some_token) = new_token {
-                        tokens.push(some_token);
-                    }
+                if let Some(some_token) = new_token {
+                    tokens.push(some_token);
                 }
             }
         }
